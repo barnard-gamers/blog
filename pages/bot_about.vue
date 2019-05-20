aec0e346f79e9545bf186facd8248866d0e99458
poyo555 <kou555.hoshi@gmail.com>
2019-05-20 10:33:31 +0900

    記事の追加

---
+++ pages/bot_about.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="main">
+    <div class="top">
+      <img class="barnard-img" src="~/assets/comet.svg" alt="">
+      <div class="barnard-title">Barnard Bots</div>
+      </div>
+    <ul class="bots">
+      <div class="bot-about">
+        <img class="bot-img" src="~/assets/pantheon-dragonslayer.png" alt="">
+        <ul>
+          <li>おそらく皆さん最初に目にするbot</li>
+          <li><span class="barnard">Barnard</span>の秩序を保つ門番Pantheon君</li>
+          <li>彼は入って来た人に規約を提示する</li>
+          <li>シンプルなボット少し面倒かもしれないけど</li>
+          <li>同意してね</li>
+        </ul>
+      </div>
+      <div class="bot-about">
+        <img class="bot-img" src="~/assets/ryze-professor.png" alt="">
+        <ul>
+          <li>彼は、様々な国籍の人がいる</li>
+          <li><span class="barnard">Barnard</span>にとっては重要な存在Ryze先生</li>
+          <li>日本語、英語、韓国語、中国語</li>
+          <li>の４言語を翻訳してくれるぞただし</li>
+          <li>少し使い方が難しいので!rzで説明が出る</li>
+          <li>それを見て使ってみよう</li>
+        </ul>
+      </div>
+      <div class="bot-about">
+        <img class="bot-img" src="~/assets/twisted-fate-pulsefire.png" alt="">
+        <ul>
+          <li>Twisted FateはLOLに関するボット</li>
+          <li>サモナーネームを入れることで</li>
+          <li>そのサモナーのマスタリーが高い</li>
+          <li>チャンピオンを教えてくれるぞ</li>
+          <li>.helpで説明を見てみよう</li>
+        </ul>
+      </div>
+      <div class="bot-about">
+        <img class="bot-img" src="~/assets/teemo-beemo.png" alt="">
+        <ul>
+          <li>ヘイトが高いであろうTeemo</li>
+          <li>彼もLOLに関するボット</li>
+          <li>彼はとってもシンプルなボット</li>
+          <li>.teemoと打つだけでランダムなチャンピオン</li>
+          <li>を提供してくれるop.ggも出してくれるので</li>
+          <li>使いたいチャンピオンがない時にでも使おう</li>
+        </ul>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<style>
+.bot-img{
+  width: 170px;
+  height: 170px;
+  border-radius: 85px;
+}
+.bot-about{
+  margin-top: 100px;
+  display: inline-flex;
+  font-family: YuGothic,'Yu Gothic','Hiragino Kaku Gothic ProN','ヒラギノ角ゴ ProN W3',sans-serif;
+  font-size: 35px;
+  list-style: none;
+  text-align: left;
+  color: white;
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,13 @@
       </div>
     <div class="info">
       <li>一緒にゲームをしたい人を探してる人は是非❗</li>
-      <iframe src="https://discordapp.com/widget?id=498488382226300958&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
-      </div> 
+      <iframe src="https://discordapp.com/widget?id=498488382226300958&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>   
+      <ul class="bot-info">
+        <li><span class="barnard">Branard</span>ではより楽しくゲームをプレイするために</li>
+        <li>様々なボットが稼働しています。</li>
+        <li>ボットたちの使い方と紹介は<nuxt-link class="barnard" to="./bot_about">こちら</nuxt-link></li>
+      </ul>   
+    </div> 
   </div>
 </template>
 
@@ -22,6 +27,15 @@ export default {
 </script>
 
 <style>
+.bot-info{
+  width: 900px;
+  padding: 0;
+  margin: 0 auto;
+}
+
+.barnard{
+  color: #E74C3C;
+}
 .barnard-img{
   margin-top: 120px;
   height: 350px;
@@ -34,6 +48,9 @@ export default {
 body{
   margin: 0;
   background-color: #212121;
+}
+ul{
+  list-style: none;
 }
 .main{
   font-family: YuGothic,'Yu Gothic','Hiragino Kaku Gothic ProN','ヒラギノ角ゴ ProN W3',sans-serif;
